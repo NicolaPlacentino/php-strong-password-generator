0c9564549284dff0ddfcc71c08cdf00cb7c0cb2f
NicolaPlacentino <nyko0921@gmail.com>
2023-02-08 15:47:16 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!-- Oggi creeremo una pagina in grado di generare una password per gli utenti.
+L'utente potrà scegliere la lunghezza password e ricevere in un alert una password con il numero di caratteri casuali da lui richiesto!
+
+Milestone 1: creare un form che invii in GET la lunghezza della password.
+Una nostra funzione utilizzerà questo dato per generare una password casuale
+(composta da lettere, lettere maiuscole, numeri e simboli) da restituire all’utente.
+Scriviamo tutto (logica e layout) in un unico file index.php
+
+Milestone 2: verificato il corretto funzionamento del nostro codice, spostiamo la logica in un file functions.php
+che includeremo poi nella pagina principale
+
+Milestone 3: invece di visualizzare la password nella index, 
+effettuare un redirect ad una pagina dedicata che tramite $_SESSION recupererà la password da mostrare all’utente. -->
+
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Nicola Placentino">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <title>Password Generator</title>
+</head>
+<body>
+
+</body>
+</html>
