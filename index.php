74454218ce8c8d1582cdebe8205a08849e1b6961
NicolaPlacentino <nyko0921@gmail.com>
2023-02-08 15:52:59 +0100

    added static html

--- index.php
+++ index.php
@@ -26,7 +26,20 @@ effettuare un redirect ad una pagina dedicata che tramite $_SESSION recupererà 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title>Password Generator</title>
 </head>
-<body>
-
+<body class="vh-100">
+    <div class="bg-dark text-white text-center h-100 p-5">
+        <h1 class="mb-5">Password Generator</h1>
+        <div class="alert alert-info mx-auto w-25" role="alert">
+        La tua password è: <b></b>
+        </div>
+        <div class="m-5">
+            <form action="" class="input-group" method="GET">
+                <input type="number" class="form-control" placeholder="Quanti caratteri deve essere lunga la password?">
+                <button class="btn btn-primary" type="button">Invia</button>
+                <button class="btn btn-outline-secondary" type="button">Annulla</button>
+            </form>
+        </div>
+        
+    </div>
 </body>
 </html>
